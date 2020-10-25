bf6332684fb7757db718c2c6ce69990fd6b26f53
zqm64 <zqm64@outlook.com>
2020-10-25 18:42:12 +0800

    msm8937-common: Add support for rova/land/santoni

--- Android.mk
+++ Android.mk
@@ -16,7 +16,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter ulysse,$(TARGET_DEVICE)),)
+ifneq ($(filter ulysse rova land santoni,$(TARGET_DEVICE)),)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
