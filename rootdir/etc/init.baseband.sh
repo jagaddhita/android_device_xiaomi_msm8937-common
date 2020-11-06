e6e90ff16374277f778d86d1e821963cc7967df7
zqm64 <zqm64@outlook.com>
2020-11-06 15:58:08 +0800

    msm8937-common: Skip setting baseband version if already have

--- rootdir/etc/init.baseband.sh
+++ rootdir/etc/init.baseband.sh
@@ -1,7 +1,9 @@
 #!/vendor/bin/sh
 
+cur_baseband_str=$(getprop gsm.version.baseband)
+if [ -z $cur_baseband_str ]; then
 baseband_str=$(strings /vendor/firmware_mnt/image/modem.b12 | grep "MPSS.*_V" | head -1)
-
 if [ ! -z $baseband_str ]; then
     setprop gsm.version.baseband $baseband_str
+fi
 fi
