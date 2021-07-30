8e76de78cb4e2761d2586c4cfa6bb9a787c13290
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2021-07-30 20:44:15 +0800

    msm8937-common: Reconfigure tree for xiaomi msm8937 devices
    - Rebrand to msm8937
    - Drop useless stuff
    - Basic configurations

--- Android.mk
+++ Android.mk
@@ -6,7 +6,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter daisy mido sakura tissot vince ysl,$(TARGET_DEVICE)),)
+ifneq ($(filter land santoni ulysse rova tiare,$(TARGET_DEVICE)),)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
